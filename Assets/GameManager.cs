4205dab38e38d7f94c37f72e058fa88e3ec680b9
Oguzhan Tok <Osanill1901@gmail.com>
2020-07-05 13:42:57 +0300

    Unlocks skins randomly but can't do unlock animation like youtube video.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public void EndGame()
+	{
+		Debug.Log("You've unlocked all the skins.");
+		Application.Quit();
+	}
+}
